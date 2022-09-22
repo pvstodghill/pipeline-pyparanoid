9a54ad0ce16c0855551af57cc7a79b1f09822386
Paul Stodghill <paul.stodghill@usda.gov>
2022-09-22 13:42:13 -0400

    IdentifyOrthologs.py --threshold is strictly greater than.
       So --threshold 1.0 needs to be special cased.

--- doit2-orthos.bash
+++ doit2-orthos.bash
@@ -16,9 +16,24 @@ for THRESHOLD in ${ORTHO_THRESHOLDS} ; do
     echo 1>&2 "## --threshold ${THRESHOLD}"
     rm -f ${PYP}/db/all_groups.hmm.ssi
     rm -rf ${ORTHOS}/${THRESHOLD}
+    # This is an ugly hack. See https://github.com/ryanmelnyk/PyParanoid/issues/11.
+    case X${THRESHOLD}X in
+	X1X|X1.0X|X1.00X|X1.000X)
+	    THRESHOLD_ARG=
+	    ;;
+	X*X)
+	    THRESHOLD_ARG="--threshold ${THRESHOLD}"
+	    ;;
+	*)
+	    echo 1>&2 "Cannot happen."
+	    exit 1
+    esac
     IdentifyOrthologs.py --use_MP --cpus ${THREADS} \
-			 --threshold ${THRESHOLD}  \
+			 ${THRESHOLD_ARG}  \
 			 ${PYP}/db ${ORTHOS}/${THRESHOLD}
+
+    echo '## orthologs:' $(cat ${ORTHOS}/${THRESHOLD}/orthos.txt | wc -l)
+
 done
 
 # ------------------------------------------------------------------------
